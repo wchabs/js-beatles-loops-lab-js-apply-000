0358b8165a86d2d6412234dfaf20a4d7950a6caf
William Chabla <w_chabla@hotmail.com>
2018-05-23 05:16:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ function theBeatlesPlay(arrMusicians, arrInstruments){
   var tempArray = [];
   
   var i = 0;
-    for(i = 0, i < arrMusicians.length, i++){
+  
+  for(i = 0; i < arrMusicians.length; i++){
       tempArray[i] = arrMusicians[i] + " plays " + arrInstruments[i];
-    }
+  }
+    
+  return tempArray;
+}
+
+function johnLennonFacts(arrFacts){
+  var i = 0;
+  while(i < arracts.length)
 }
