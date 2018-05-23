fc837706190052bc4768f7934e749bdea0d327ed
William Chabla <w_chabla@hotmail.com>
2018-05-23 05:12:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function theBeatlesPlay(arrMusicians, arrInstruments){
   
   var i = 0;
     for(i = 0, i < arrMusicians.length, i++){
-      
+      tempArray[i] = arrMusicians[i] + " plays " + arrInstruments[i];
     }
 }
